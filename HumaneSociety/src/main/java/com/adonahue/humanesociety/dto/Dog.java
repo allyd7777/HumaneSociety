5f4c3194f0ba993419dcaf3a9fcec05bf440cbee
Brian Vang <brianvang@hotmail.com>
2020-03-11 10:00:29 -0500

    Update Dog.java

--- HumaneSociety/src/main/java/com/adonahue/humanesociety/dto/Dog.java
+++ HumaneSociety/src/main/java/com/adonahue/humanesociety/dto/Dog.java
@@ -1,9 +1,61 @@
 package com.adonahue.humanesociety.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 /**
  *
  * @author allison
  */
 public class Dog {
+    private String dogName; //Key
+    private String dogSize; //Values
+    private double dogAge; //Values
+    private BigDecimal adoptionCost; //Values
+    private LocalDate adimissionDate; //Values
+    
+    public Dog(String dogName) { //Constructor for dog name.
+        this.dogName = dogName;
+    }
+
+    public String getDogName() {
+        return dogName;
+    }
+
+    public void setDogName(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public String getDogSize() {
+        return dogSize;
+    }
+
+    public void setDogSize(String dogSize) {
+        this.dogSize = dogSize;
+    }
+
+    public double getDogAge() {
+        return dogAge;
+    }
+
+    public void setDogAge(double dogAge) {
+        this.dogAge = dogAge;
+    }
+
+    public BigDecimal getAdoptionCost() {
+        return adoptionCost;
+    }
+
+    public void setAdoptionCost(BigDecimal adoptionCost) {
+        this.adoptionCost = adoptionCost;
+    }
+
+    public LocalDate getAdimissionDate() {
+        return adimissionDate;
+    }
+
+    public void setAdimissionDate(LocalDate adimissionDate) {
+        this.adimissionDate = adimissionDate;
+    }
     
 }
