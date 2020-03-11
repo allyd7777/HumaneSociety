7585a88a3cbd6c8ff28b38d2b0945be5b7e4b546
Colin Berry <colin.p.berry3@gmail.com>
2020-03-11 12:43:29 -0500

    spring confusion but pushing anyway

--- HumaneSociety/src/main/java/com/adonahue/humanesociety/App.java
+++ HumaneSociety/src/main/java/com/adonahue/humanesociety/App.java
@@ -1,9 +1,19 @@
 package com.adonahue.humanesociety;
 
+import java.lang.ModuleLayer.Controller;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  *
  * @author allison
  */
 public class App {
     
+    public static void main(String[] args) {
+        
+         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+         Controller controller = ctx.getBean("controller", Controller.class);
+        controller.run();
+    }
 }
